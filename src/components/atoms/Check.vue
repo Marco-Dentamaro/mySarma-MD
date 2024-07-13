373e618ee9a3a475e1a473afb99db24e6aaf5ff8
Marco Dentamaro <marco.dentamaro@aulab.it>
2024-07-13 21:08:25 +0200

    terminato hero ma occhio al responsive da schermi xxl

---
+++ src/components/atoms/Check.vue
@@ -0,0 +1,9 @@
+<template>
+<svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="10" cy="10.3564" r="10" fill="#112340"/>
+<path d="M5.26245 10.0234L8.63087 13.3568L15.2625 7.35678" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+
+
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,13 @@ export default {
       colors :{
         greenCustom : '#abe8dc',
         blueNav : '#2b50b0',
-      }
+        bgCard : '#E6ECF9',
+        primaryCust : '#2B50B0',
+      },
+
+      backgroundImage:{
+        bgHero: "linear-gradient(rgba(43, 80, 176, 0.4), rgba(43, 80, 176,0.4)), url('./src/media/heroImg.png')",
+      },
     },
   },
   plugins: [],
